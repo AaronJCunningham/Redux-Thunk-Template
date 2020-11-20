8efe92b8883e2dd4189dee93c48df802c579e9a3
Aaron J Cunningham <aaronjosephcunningham@gmail.com>
2020-11-20 13:14:06 +0100

    2nd

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {increment, fetchPosts} from '../actions'
+import { connect } from 'react-redux'
+
+const App = ({count, posts, increment, fetchPosts}) => {
+console.log(posts)
+return <>
+<h1>Counter {count}</h1>
+<button onClick={increment}>+</button>
+
+<button onClick={fetchPosts}>Fetch Posts</button>
+<h1>Posts </h1>
+{posts.map((post) => <h1>{post.title}</h1>)}
+</>
+}
+
+const mapStateToProps = (state) => {
+  return { count: state.count,
+  posts: state.posts.data}
+  
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: () => dispatch(increment()),
+        fetchPosts: () => dispatch(fetchPosts())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import counterReducer from './counterReducer'
+import fetchPosts from './postsReducer'
+
+const rootReducer = combineReducers({
+    count: counterReducer,
+    posts: fetchPosts,
+})
+
+export default rootReducer;
